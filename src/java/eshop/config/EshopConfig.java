6deb7ac98f0c83a8c55fd4411cef879e0aed2a0c
Ebuka <39867447+cosecruz@users.noreply.github.com>
2018-07-02 17:13:49 +0100

    added some config files

---
+++ src/java/eshop/config/EshopConfig.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eshop.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "eshop")
+/**
+ *
+ * @author EBUKA
+ */
+public class EshopConfig {
+    
+}
---
+++ src/java/eshop/config/EshopInit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eshop.config;
+
+import com.sun.faces.config.FacesInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+/**
+ *
+ * @author EBUKA
+ */
+public class EshopInit extends FacesInitializer implements WebApplicationInitializer{
+
+    @Override
+    public void onStartup(ServletContext sc) throws ServletException {
+        
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(EshopConfig.class, HibernateConfig.class);
+        sc.addListener(new ContextLoaderListener(appContext));
+    }
+}
+    
+
